d768b7368effc99510ac2dea114c71471d39ae21
StepanyukAN <fargogi@mail.ru>
2019-02-04 21:10:01 +1000

    Сделал все, что было на уроке + поменял ссылки на картинки

---
+++ MyWebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWebStore.Models;
+
+namespace MyWebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ObservableCollection<EmployeeView> _employees = new ObservableCollection<EmployeeView>
+        {
+            new EmployeeView
+            {
+                ID = 1,
+                FirstName = "Иван",
+                Patronymic="Иванович",
+                SurName="Иванов",
+                Age=44
+
+            },
+             new EmployeeView
+            {
+                ID = 2,
+                FirstName = "Петр",
+                Patronymic="Петрович",
+                SurName="Петров",
+                Age=57
+
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var employee = _employees.FirstOrDefault(e => e.ID.Equals(id));
+            return View(employee);
+        }
+    }
+}
--- MyWebStore/Controllers/HomeController.cs
+++ MyWebStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,39 +10,15 @@ namespace MyWebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<EmployeeView> _employees = new List<EmployeeView>
-        {
-            new EmployeeView
-            {
-                ID = 1,
-                FirstName = "Иван",
-                Patronymic="Иванович",
-                SurName="Иванов",
-                Age=44
-
-            },
-             new EmployeeView
-            {
-                ID = 2,
-                FirstName = "Петр",
-                Patronymic="Петрович",
-                SurName="Петров",
-                Age=57
-
-            }
-        };
-
-        public IActionResult Details(int id)
-        {
-            var employee = (from e in _employees
-                            where e.ID == id
-                            select e).ToArray()[0];
-            return View(employee);
-        }
-
-        public IActionResult Index()
-        {
-            return View(_employees);
-        }
+        public IActionResult Index() => View();
+        public IActionResult Shop() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Login() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult CheckOut() => View();
+        public IActionResult Cart() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Blog() => View();
+        public IActionResult ErrorPage404() => View();
     }
 }
