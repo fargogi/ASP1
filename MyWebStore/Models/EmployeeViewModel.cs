a33346907ceab19b5b5aba123b4e549ed9200b6d
StepanyukAN <fargogi@mail.ru>
2019-02-09 19:57:31 +1000

    Первая половина урока

--- MyWebStore/Models/EmployeeViewModel.cs
+++ MyWebStore/Models/EmployeeViewModel.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyWebStore.Models
 {
     public class EmployeeViewModel
     {
         public int ID { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Имя должно быть обязательно.")]
+        [RegularExpression("^[A-ZА-Я][a-z,а-я]*",ErrorMessage ="Имя должно начинаться с заглавной буквы и содержать только буквы")]
+        [Display(Name ="Имя сотрудника")]
         public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Фамилия тоже обязательна")]
+        [RegularExpression("^[A-ZА-Я][a-z,а-я]*", ErrorMessage = "Фамилия должна начинаться с заглавной буквы и содержать только буквы")]
+
+        [Display(Name = "Фамилия сотрудника")]
         public string SurName { get; set; }
+
+        [Display(Name = "Отчество сотрудника")]
         public string Patronymic { get; set; }
+
+        [Required(ErrorMessage ="Не указан возраст.")]
+        [Range(minimum:18, maximum:150,ErrorMessage = "Возраст должен быть в диапазоне 18 - 150 лет")]
         public int Age { get; set; }
     }
 }
