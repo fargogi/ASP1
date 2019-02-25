40e22794ebcedf00f6244847f33078e3f62d0e03
StepanyukAN <fargogi@mail.ru>
2019-02-25 18:21:14 +1000

    Создана область администратора

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -90,6 +90,9 @@ namespace MyWebStore
                 routes.MapRoute(
                                 name: "default", 
                                 template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                                name: "areas",
+                                template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 
             });
